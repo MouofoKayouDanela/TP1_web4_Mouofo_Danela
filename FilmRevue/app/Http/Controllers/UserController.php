e26ca918c1d0c5e8cdb50cd13ab6ab2ce9e9de16
MouofoKayouDanela <mouofodanela@gmail.com>
2025-12-01 12:47:01 -0500

    creation user

--- FilmRevue/app/Http/Controllers/UserController.php
+++ FilmRevue/app/Http/Controllers/UserController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Database\QueryException;
+use App\Http\Resources\UserResource;
+
 
 class UserController extends Controller
 {
@@ -16,10 +22,37 @@ class UserController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     * https://laravel.com/docs/master/validation#creating-form-requests
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validated = $request->validate([
+                'first_name'      => 'required|string|max:50',
+                'last_name'      => 'required|string|max:50',
+                'login'      => 'required|string|max:50',
+                'email'     => 'required|email|unique:users,email|max:50',
+                'password'  => 'required|string|min:6|max:255',
+            ]);
+
+            $user = User::create([
+                'first_name'     => $validated['first_name'],
+                'last_name'     => $validated['last_name'],
+                'login'     => $validated['login'],
+                'email'    => $validated['email'],
+                'password' => Hash::make($validated['password']),
+            ]);
+
+            return (new UserResource($user))
+                ->response()
+                ->setStatusCode(201);
+
+        } catch (ValidationException $ex) {
+            abort(422, $ex->getMessage());
+        
+        } catch (\Exception $ex) {
+            abort(500, "Server error");
+        }
     }
 
     /**
